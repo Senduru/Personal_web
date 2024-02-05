9833f35cc1c64354c72eda822d595325f2418760
Senduru <153762080+Senduru@users.noreply.github.com>
2024-02-05 18:38:13 +0530

    Add files via upload

---
+++ downloadCV.js
@@ -0,0 +1,21 @@
+// document.getElementById('downloadButton').addEventListener('click', function() {
+//     // Replace 'path/to/your/cv.pdf' with the actual path to your CV file
+//     // var fileUrl = 'assert/senduru.pdf';
+//     var btn=new Audio("sounds/download.mp3");
+//     btn.play();
+//     // Create a virtual anchor element
+//     var a = document.createElement('a');
+
+//     // Set the download attribute and file URL
+//     alert("The CV as Successfully downloaded")
+//     a.setAttribute('download', 'assert/senduru.pdf');
+//     a.href = fileUrl;
+    
+//     // Append the anchor to the body and trigger a click event
+//     document.body.appendChild(a);
+//     a.click();
+
+//     // Remove the anchor from the body
+//     document.body.removeChild(a);
+    
+// });
